d304f00096b4269307276ecdbbdea34641b59bdf
Fabian Mattig <fabian.mattig@pm.me>
2026-06-02 13:26:56 +0200

    Version Bump

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName("ASTRA");
-    app.setApplicationVersion("0.2.0");
+    app.setApplicationVersion("0.2.1");
     app.setOrganizationName("ASTRA");
 
     QFontDatabase::addApplicationFont(":/fonts/FiraCode-Regular.ttf");
--- src/views/MainWindow.cpp
+++ src/views/MainWindow.cpp
@@ -219,7 +219,7 @@ void MainWindow::createActions()
     connect(_aboutAction, &QAction::triggered, [this]() {
         QMessageBox::about(this, "About ASTRA",
             "ASTRA - Advanced STellar astrophysics Research and Analysis tool\n\n"
-            "Version 0.2.0\n\n"
+            "Version 0.2.1\n\n"
             "A modern Qt6 application for managing and analyzing stellar astrophysics data.\n"
             "All your data in one place! :)");
     });
